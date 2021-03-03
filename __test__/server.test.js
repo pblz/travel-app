83d07887906e2b2f48866c17759a12495c9ccad1
Pia Balz <pia.balz@daimler.com>
2021-03-03 09:33:31 +0100

    Fixed server side tests

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,26 +1,21 @@
 
-const app = require("../src/server/index.js");
-const request = require("supertest");
+const app = require('../src/server/server.js')
+const supertest = require('supertest')
+const request = supertest(app)
 
 describe("Testing express server", () => {
 
-    test("Respond to root get", done => {
-        request(app)
-          .get("/")
-          .then(response => {
-            expect(response.statusCode).toBe(200);
-            done();
-          });
-      });
+  test("Respond to root get", async done => {
+    const response = await request.get('/')
+    expect(response.statusCode).toBe(200);
+    done();
+  });
 
-      test("Respond to post Weather", done => {
-        request(app)
-          .post("/weather")
-          .then(response => {
-            expect(response.statusCode).toBe(200);
-            expect(response.type).toEqual('application/json');
-            done();
-          });
-      });
+  test("Respond to weather post", async done => {
+    const response = await request.post('/weather')
+    expect(response.statusCode).toBe(200);
+    done();
+  });
 });
+
 
